ebf1c82a74e39eb906e216e2c9f787758ca8e1d6
Gleb Filimonov <gleb.filimonov@sap.com>
2021-08-03 16:28:22 +0200

    Add properties

--- src/models/expense.ts
+++ src/models/expense.ts
@@ -1,8 +1,10 @@
 export interface Expense {
     id: string;
     categoryId: string;
+    categoryName: string;
     price: number;
     quantity: number;
     currency: string;
     comment: string;
+    date: Date;
 }
